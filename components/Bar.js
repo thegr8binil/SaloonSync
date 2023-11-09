95d24de2499f183c5545d7da1faac5f94cbaeb29
thegr8binil <binil.b.re@gmail.com>
2023-11-09 17:45:51 +0530

    main

--- components/Bar.js
+++ components/Bar.js
@@ -27,7 +27,7 @@ export const Bar = () => {
       <div className="bg-blue-500 shadow-2xl shadow-blue-500 flex flex-col gap-2 items-center justify-between max-w-10xl px-5 rounded-2xl py-4 mx-6 md:flex-row">
         <div className="flex gap-3 p-5 rounded-3xl mt-3">
           <div className="flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-semibold text-white">
+            <h1 className="text-3xl font-semibold text-white text-center">
               Employee Management
             </h1>
             <p className="font-normal text-white text-center text-sm mt-3">
@@ -38,7 +38,7 @@ export const Bar = () => {
         </div>
         <div className="flex gap-3 p-5 rounded-3xl mt-3">
           <div className="flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-semibold text-white">
+            <h1 className="text-3xl font-semibold text-white text-center">
               Inventory Management
             </h1>
             <p className="font-normal text-white text-center text-sm mt-3">
@@ -48,8 +48,8 @@ export const Bar = () => {
           </div>
         </div>
         <div className="flex gap-3 p-5 rounded-3xl mt-3">
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-semibold text-white">
+          <div className="flex flex-col items-center justify-center ">
+            <h1 className="text-3xl font-semibold text-white text-center">
               Multiple Salon Management
             </h1>
             <p className="font-normal text-white text-center text-sm mt-3">
